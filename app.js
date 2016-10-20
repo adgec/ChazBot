2e1ac1f713a0bfa638b9fcbf6da78286242b26ea
James Gifford <jamesgifford88@gmail.com>
2016-10-20 21:26:30 +0100

    Added bot appid and pwd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: '61764270-8a22-410a-b840-dadb61ae9a80', appPassword: 'CS68Q12SY0J1UNmw5vEOx05' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
